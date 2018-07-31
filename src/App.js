b74eb2de799d7f945ba30f7ea1c7253570fa65a9
JarekZ92 <z.jarek92@gmail.com>
2018-07-31 19:52:09 +0200

    added counter function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react"
 
 class App extends Component {
+    state = {
+        startCounter: 0
+    }
+    addCounterFunction = () => {
+        this.setState({
+            startCounter: this.state.startCounter + 1
+        })
+    }
 
+    
 }
 
 export default App
