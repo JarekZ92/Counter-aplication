1e77428d86457afbc332d7788008d9882b762076
JarekZ92 <z.jarek92@gmail.com>
2018-08-01 13:15:17 +0200

    Added function that add 5 numbers

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 class App extends Component {
-    state = {
-        startCounter: 0
-    }
-    addCounterFunction = () => {
-        this.setState({
-            startCounter: this.state.startCounter + 1
-        })
-    }
+  state = {
+    startCounter: 0,
+  };
 
-    subtractCounterFunction = () => {
-        this.setState({
-            startCounter: this.state.startCounter -1
-        })
-    }
+  addCounterFunction = () => {
+    this.setState({
+      startCounter: this.state.startCounter + 1
+    });
+  };
 
-    render () {
-        return(
-            <div>
-            <h1>{this.state.startCounter}</h1>
-            <button onClick ={this.addCounterFunction}> - </button>
-            <button onClick ={this.subtractCounterFunction}> + </button>
+  subtractCounterFunction = () => {
+    this.setState({
+      startCounter: this.state.startCounter - 1
+    });
+  };
 
-
-            </div>
-        )
-    }
+  addFiveFunction = () => {
+    this.setState({
+      startCounter: this.state.startCounter +5
+    })
+  }
 
 
+  render() {
+    return (
+      <div>
+        <h1>{this.state.startCounter}</h1>
+        <button onClick={this.addCounterFunction}> + </button>
+        <button onClick={this.subtractCounterFunction}> - </button>
+        <button onClick={this.addFiveFunction}> +5 </button>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
